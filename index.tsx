4c227455b27b1dcb8fa4ae49cb8ede5f12a47968
Dhruv Daberao <dhruvdaberao@gmail.com>
2025-09-16 17:24:28 +0530

    lol

--- index.tsx
+++ index.tsx
@@ -1,31 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import { ThemeProvider } from './contexts/ThemeContext';
-// import { AuthProvider } from './contexts/AuthContext';
-// import { SocketProvider } from '@/contexts/SocketContext';
-
-// const rootElement = document.getElementById('root');
-// if (!rootElement) {
-//   throw new Error("Could not find root element to mount to");
-// }
-
-// const root = ReactDOM.createRoot(rootElement);
-// root.render(
-//   <React.StrictMode>
-//     <ThemeProvider>
-//       <AuthProvider>
-//         <SocketProvider>
-//           <App />
-//         </SocketProvider>
-//       </AuthProvider>
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
-
-
-
-
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
@@ -36,7 +10,7 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
