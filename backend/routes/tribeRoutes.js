f9a9070d3486de645f158a29355eabbe18cbb53e
Dhruv Daberao <dhruvdaberao@gmail.com>
2025-09-16 12:10:47 +0530

    corrected tribeRoute

--- backend/routes/tribeRoutes.js
+++ backend/routes/tribeRoutes.js
@@ -4,11 +4,12 @@ import { useAuth } from './AuthContext';
 
 const SocketContext = createContext<Socket | null>(null);
 
-export const useSocket = (): Socket | null => {
+export const useSocket = () => {
   return useContext(SocketContext);
 };
 
-export const SocketProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+
+export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const { currentUser } = useAuth();
 
